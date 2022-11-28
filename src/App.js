abb788f4a5f47e01b9cfddf857d22a875ec7c117
Gogulan12 <gogulanravi12@gmail.com>
2022-11-28 12:20:01 -0500

    added h2 and 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello This is the production branch</h1>
+      <h2>tried this wea react app before</h2>
+      <h3>This is so easy</h3>
     </div>
   );
 }
